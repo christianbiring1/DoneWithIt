33c97eef8a5a3754e779dda09191b4564e2c074d
christian <christianbiringanine22@gmail.com>
2023-09-19 19:31:59 +0200

    Building the Button component

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Platform } from 'react-native';
+import { StyleSheet, View, Platform, Text } from 'react-native';
 import AppText from './app/components/AppText/AppText';
 
 import WelcomeScreen from './app/screen/WelcomeScreen';
 import ViewImageScreen from './app/screen/ViewImageScreen';
+import AppButton from './app/components/AppButton';
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
     // <WelcomeScreen />
     <View style={styles.container}>
       <AppText >I love React Native</AppText>
+      <AppButton title="Login" onPress={() => console.log('tapped')}/>
+      <Text>What happen</Text>
     </View>
   );
 }
