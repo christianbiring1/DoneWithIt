1b7d99d37dd82490a7636e5a7d57f7a46896e0ef
christian <christianbiringanine22@gmail.com>
2023-09-19 16:07:35 +0200

    Building the View Image Screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Platform, Dimensions } from 'react-native';
 import WelcomeScreen from './app/screen/WelcomeScreen';
+import ViewImageScreen from './app/screen/ViewImageScreen';
 
 export default function App() {
   // console.log(Dimensions.get("window"));
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   );
 }
 
