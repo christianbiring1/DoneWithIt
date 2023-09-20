3f9923eb513216017d0e2b22471410d1eac5bf3f
christian <christianbiringanine22@gmail.com>
2023-09-20 14:06:22 +0200

    Handling Swipes on ListItems

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Platform, Text } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
 import AppText from './app/components/AppText/AppText';
+import AppButton from './app/components/AppButton';
+import Card from './app/components/Card';
 
 import WelcomeScreen from './app/screen/WelcomeScreen';
 import ViewImageScreen from './app/screen/ViewImageScreen';
-import AppButton from './app/components/AppButton';
-import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screen/ListingDetailsScreen';
 import MessagesScreen from './app/screen/MessagesScreen';
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <GestureHandlerRootView>
+      <MessagesScreen />
+    </GestureHandlerRootView>
     // <ViewImageScreen />
     // <WelcomeScreen />
     // <View style={{
--- app/screen/MessagesScreen.js
+++ app/screen/MessagesScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
 import ListItemSeparator from '../components/ListItemSeparator';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
 
 const messages = [
@@ -33,6 +34,7 @@ function MessagesScreen(props) {
               subTitle={item.description}
               image={item.image}
               onPress={() => console.log('Message Selected', item)}
+              renderRightActions={ListItemDeleteAction}
             />
           }
           ItemSeparatorComponent={ListItemSeparator}
