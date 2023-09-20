3e895906c267690b4508f6558474d180252d5b9f
christian <christianbiringanine22@gmail.com>
2023-09-20 17:53:03 +0200

    Extending the ListItem Component

--- App.js
+++ App.js
@@ -6,40 +6,30 @@ import AppText from './app/components/AppText/AppText';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import Icon from './app/components/Icon';
+import Screen from './app/components/Screen';
 
 import WelcomeScreen from './app/screen/WelcomeScreen';
 import ViewImageScreen from './app/screen/ViewImageScreen';
 import ListingDetailsScreen from './app/screen/ListingDetailsScreen';
 import MessagesScreen from './app/screen/MessagesScreen';
+import ListItem from './app/components/ListItem';
 
 export default function App() {
   return (
     <GestureHandlerRootView>
-      <MessagesScreen />
-      <Icon name="email" size={100}/>
+      <Screen>
+        <ListItem
+          title="My title"
+          // subTitle="My subTitle"
+          ImageComponent={<Icon name="email" />}
+        />
+      </Screen>
     </GestureHandlerRootView>
+      // <MessagesScreen />
+      // <Icon name="email" size={100}/>
     // <ViewImageScreen />
     // <WelcomeScreen />
-    // <View style={{
-    //   backgroundColor: "#f8f4f4",
-    //   padding: 20,
-    //   paddingTop: 100
-    // }}>
-    //   <Card
-    //     title="Red jacket for sale"
-    //     subTitle="$100"
-    //     image={require("./app/assets/chris.jpg")}
-    //   />
-    // </View>
     // <ListingDetailsScreen />
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: "center",
-//     paddingTop: Platform.OS === 'android' ? 50 : 0
-//   }
-// })
 
