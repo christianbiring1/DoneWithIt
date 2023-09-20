e0e8219bd094774685ba4b2808e8ef9ad2ea1e45
christian <christianbiringanine22@gmail.com>
2023-09-20 12:05:49 +0200

    FlatList

--- App.js
+++ App.js
@@ -7,10 +7,12 @@ import ViewImageScreen from './app/screen/ViewImageScreen';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screen/ListingDetailsScreen';
+import MessagesScreen from './app/screen/MessagesScreen';
 
 export default function App() {
   return (
-    <ViewImageScreen />
+    <MessagesScreen />
+    // <ViewImageScreen />
     // <WelcomeScreen />
     // <View style={{
     //   backgroundColor: "#f8f4f4",
