4c49817fc07f0d96cc1c92e3fe9f042448bd0a7d
christian <christianbiringanine22@gmail.com>
2023-09-20 11:18:25 +0200

    Building the Listing Details Screen

--- App.js
+++ App.js
@@ -6,22 +6,24 @@ import WelcomeScreen from './app/screen/WelcomeScreen';
 import ViewImageScreen from './app/screen/ViewImageScreen';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screen/ListingDetailsScreen';
 
 export default function App() {
   return (
     // <ViewImageScreen />
     // <WelcomeScreen />
-    <View style={{
-      backgroundColor: "#f8f4f4",
-      padding: 20,
-      paddingTop: 100
-    }}>
-      <Card
-        title="Red jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/chris.jpg")}
-      />
-    </View>
+    // <View style={{
+    //   backgroundColor: "#f8f4f4",
+    //   padding: 20,
+    //   paddingTop: 100
+    // }}>
+    //   <Card
+    //     title="Red jacket for sale"
+    //     subTitle="$100"
+    //     image={require("./app/assets/chris.jpg")}
+    //   />
+    // </View>
+    <ListingDetailsScreen />
   );
 }
 
