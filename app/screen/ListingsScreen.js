4ec953f1a0a597c3ee0b1e825f562de70dfb81f4
christian <christianbiringanine22@gmail.com>
2023-09-20 19:41:39 +0200

    Building the Listings Screen

--- app/screen/ListingsScreen.js
+++ app/screen/ListingsScreen.js
@@ -9,15 +9,33 @@ import colors from '../config/colors';
 const listings = [
   {
     id: 1,
-    title: 'Red jacket for sale',
+    title: 'Blue gray jacket for sale',
     price: 100,
-    image: require('../assets/chris.jpg')
+    image: require('../assets/jacket.webp')
   },
   {
     id: 2,
+    title: 'Aged couch in somehow good shap :-)',
+    price: 1000,
+    image: require('../assets/couch-aged.webp')
+  },
+  {
+    id: 3,
     title: 'Couch in great condition',
     price: 1000,
-    image: require('../assets/chris.jpg')
+    image: require('../assets/Couch.webp')
+  },
+  {
+    id: 4,
+    title: 'A small saloon',
+    price: 1000,
+    image: require('../assets/chair-image.jpeg')
+  },
+  {
+    id: 5,
+    title: 'A blue jacket for sale',
+    price: 80,
+    image: require('../assets/blue-jacket.webp')
   }
 ]
 
@@ -42,6 +60,7 @@ function ListingsScreen(props) {
 const styles = StyleSheet.create({
   screen: {
     padding: 20,
+    marginTop: 10,
     backgroundColor: colors.light
   }
 })
