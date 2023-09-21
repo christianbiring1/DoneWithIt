1b66e37d5a10052b6ed415ddddf9186617b6ec3c
christian <christianbiringanine22@gmail.com>
2023-09-21 18:12:55 +0200

    Fixing the placeholder colors

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -18,7 +18,11 @@ function AppPicker({ icon, items, onSelectItem, placeholder, selectedItem }) {
               style={styles.icon}
             />
           }
-          <AppText style={styles.text}>{selectedItem ? selectedItem.label : placeholder}</AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ): (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
           <MaterialCommunityIcons name={ modalVisible ? "chevron-up" : "chevron-down"} size={20} color={defaultStyles.colors.medium}/>
         </View>
       </TouchableWithoutFeedback>
@@ -53,6 +57,10 @@ const styles = StyleSheet.create({
   },
   icon: {
     marginRight: 10
+  },
+  placeholder: {
+    color: defaultStyles.colors.medium,
+    flex: 1,
   },
   text: {
     flex: 1,
